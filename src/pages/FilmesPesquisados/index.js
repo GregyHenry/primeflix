f4269ffae945cc4221da5c53eb8c9448cd97cce6
Gregory Henry <ryguimaraes@gmail.com>
2023-05-13 23:24:52 -0300

    remove import

--- src/pages/FilmesPesquisados/index.js
+++ src/pages/FilmesPesquisados/index.js
@@ -1,6 +1,5 @@
 import {Link} from "react-router-dom";
 import {useEffect, useState} from "react";
-import "./filmes-pesquisados.css";
 
 function FilmesPesquisados() {
   const [filmes, setFilmes] = useState([]);
